d2b1ea9fafe9f0c40b7c1126daa6135f80f5fca6
Geovanna126 <novaisgeovanna260@gmail.com>
2026-06-21 19:18:04 -0300

    Update validacao.js

--- validacao.js
+++ validacao.js
@@ -1,33 +1,33 @@
 document.querySelectorAll("form").forEach(formulario => {
-  formulario.addEventListener("submmit", (evento) => {
-    const nome = formulario.querySelector('[name=^"nome"]')?.value.trim();
-    const email = formulario.querySelector('[type=^"email"]')?.value.trim();
-    const senha = formulario.querySelector('[name=^"senha"]')?.value;
-    const confirmar = formulario.querySelector('[name=^"senha" i]'):not([name="senha])')?.value;
+    formulario.addEventListener("submit", (evento) => {
+  
+        const nome = formulario.querySelector('[name^="nome"]')?.value.trim();
+        const email = formulario.querySelector('[type="email"]')?.value.trim();
+        const senha = formulario.querySelector('[name="senha"]')?.value;
+        const confirmar = formulario.querySelector('[name*="senha" i]:not([name="senha"])')?.value;
 
-    if (nome && nome.split(/\s+/).length < 2) {
-      alert("Insira seu nome completo, nome e sobrenome");
-      evento.preventDefault();
-      return;
-    }
+        if (nome && nome.split(/\s+/).length < 2) {
+            alert("Por favor, insira seu nome completo (Nome e Sobrenome).");
+            evento.preventDefault(); // Trava o envio para o PHP se houver erro
+            return;
+        }
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (email && !emailRegex.test(email)) {
-      alert("Insira um e-mail válido");
-      evento.preventDefault();
-      return;
-    }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (email && !emailRegex.test(email)) {
+            alert("Por favor, insira um e-mail válido.");
+            evento.preventDefault();
+            return;
+        }
 
-    if (senha && senha.length < 8) {
-      alert("A senha deve ter no mínimo 8 caracteres");
-      evento.preventDefault();
-      return;
-    }
+        if (senha && senha.length < 8) {
+            alert("A senha deve conter no mínimo 6 caracteres.");
+            evento.preventDefault();
+            return;
+        }
 
-    if (senha && confirmar && senha!== confirmar) {
-    alert ("Senha incorreta");
-      evento.preventDefault();
-    
-  }
-}
-)
+        if (senha && confirmar && senha !== confirmar) {
+            alert("As senhas não coincidem!");
+            evento.preventDefault();
+        }
+    });
+});
